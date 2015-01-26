9667017dd1b74769b365c1e822215d8b69d949cb
迹 <366006820@qq.com>
2015-01-26 12:02:51 +0800

    change  some code

--- VolumeControl/VolumeControl/MainWindow.xaml.cs
+++ VolumeControl/VolumeControl/MainWindow.xaml.cs
@@ -29,18 +29,48 @@ namespace VolumeControl
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            this.currenttxt.Text = SPVolumeControl.Instance.VolumeCurrent.ToString();
-            this.systxt.Text = SPVolumeControl.Instance.VolumeSystem.ToString();
+           this.currenttxt.Value = SPVolumeControl.Instance.VolumeCurrent;
+           this.systxt.Value= SPVolumeControl.Instance.VolumeSystem;
         }
+
+       
+
+        public double SystemValue
+        {
+            get { return (double)GetValue(SystemValueProperty); }
+            set { SetValue(SystemValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SystemValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SystemValueProperty =
+            DependencyProperty.Register("SystemValue", typeof(double), typeof(MainWindow), new PropertyMetadata(0d,PropertyChangedCallback));
+
+
+
+        static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var de = (double)e.NewValue;
+            SPVolumeControl.Instance.VolumeSystem = (float)de;
+        }
+        public double CurrentVolumeValue
+        {
+            get { return (double)GetValue(CurrentVolumeValueProperty); }
+            set { SetValue(CurrentVolumeValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CurrentVolumeValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentVolumeValueProperty =
+            DependencyProperty.Register("CurrentVolumeValue", typeof(double), typeof(MainWindow), new PropertyMetadata(0d,PropertyChangedCallback));
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            SPVolumeControl.Instance.VolumeCurrent = (float.Parse(this.currenttxt.Text));
+            SPVolumeControl.Instance.VolumeCurrent = (float.Parse(this.currenttxt.Value.ToString()));
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            SPVolumeControl.Instance.VolumeSystem = (float.Parse(this.systxt.Text));
+            SPVolumeControl.Instance.VolumeSystem = (float.Parse(this.systxt.Value.ToString()));
         }
     }
 }
